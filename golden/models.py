2aec1ead3bca0c93beed32af724a9ad4b64deba6
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-16 22:25:06 +0300

    third migration

--- golden/models.py
+++ golden/models.py
@@ -18,6 +18,28 @@ class Project(models.Model):
     user = models.ForeignKey(User, null=True)
     profile = models.ForeignKey(Profile,null=True,on_delete=models.CASCADE)
 
+class Reviews(models.Model):
+    REVIEW_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+    design = models.IntegerField(choices=REVIEW_CHOICES,default=0,blank=False)
+    usability = models.IntegerField(choices=REVIEW_CHOICES,default=0,blank=False)
+    content = models.IntegerField(choices=REVIEW_CHOICES,default=0,blank=False)
+    average =  models.DecimalField(default=1,blank=False,decimal_places=2,max_digits=100)
+    project = models.ForeignKey(Project,null=True,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,null=True,blank=True)
+
+
+
 
 
 
