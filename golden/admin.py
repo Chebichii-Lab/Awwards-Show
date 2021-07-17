25f99c74bc444e22c720bb9c0c6a088a03ced8e1
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-17 23:40:26 +0300

    add admin credentials in the admin.py

--- golden/admin.py
+++ golden/admin.py
@@ -1,3 +1,7 @@
+from golden.models import Profile, Project, Reviews
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Reviews)
